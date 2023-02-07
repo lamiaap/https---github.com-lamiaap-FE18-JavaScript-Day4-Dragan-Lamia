3563c0a86d8b16476332533666a7c38d9401c6ff
lamiaap <lamiaap2001@gmail.com>
2023-02-07 13:23:59 +0100

    ex1

---
+++ java4.js
@@ -0,0 +1,38 @@
+function printInfo() {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let age = document.getElementById("age").value;
+    let job = document.getElementById("job").value;
+    document.getElementById("result").innerHTML = `my name is <span id='color'>${fname}</span>  ${lname} and im  ${age} years old and im working on ${job}` ;
+    if (fname.length > 5) {
+        document.getElementById("color").style.color = "green";
+    } else {
+        document.getElementById("color").style.color = "red";
+    }
+    if (job == "IT") {
+        document.getElementById("result").style.backgroundColor = "purple";
+    }
+    else if(job == "Hospitality") {
+        document.getElementById("result").style.backgroundColor = "yellow";
+    }
+    console.log(fname, lname, age);
+}
+document.getElementById("btn").addEventListener("click", printInfo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
